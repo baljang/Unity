49afdb10296730f6d91161107892e31f29fe30a3
baljang <baljang7@gmail.com>
2022-08-29 16:37:09 +0900

    05_02_Camera_Camera#2
    Q1. 땅을 클릭하면 이동 하는 것을 구현해 보아라.
    
    Q2. 마지막에 와리가리 하는 현상을 해결해 보아라.

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -5,31 +5,31 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField]
-    Define.CameraMode _mode = Define.CameraMode.QuaterView;
+    Define.CameraMode _mode = Define.CameraMode.QuarterView;
 
     [SerializeField]
     Vector3 _delta = new Vector3(0.0f, 6.0f, -5.0f);
 
     [SerializeField]
-    GameObject _player = null; 
+    GameObject _player = null;
 
-     void Start()
+    void Start()
     {
-        
+
     }
 
     void LateUpdate()
     {
-        if(_mode == Define.CameraMode.QuaterView)
+        if (_mode == Define.CameraMode.QuarterView)
         {
             transform.position = _player.transform.position + _delta;
             transform.LookAt(_player.transform);
         }
     }
 
-    public void SetQuaterView(Vector3 delta)
+    public void SetQuarterView(Vector3 delta)
     {
-        _mode = Define.CameraMode.QuaterView; 
+        _mode = Define.CameraMode.QuarterView;
         _delta = delta;
     }
 }
--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    float _speed = 10.0f; 
+    float _speed = 10.0f;
+
+    bool _moveToDest = false; 
+    Vector3 _destPos; 
+
      void Start()
     {
         Managers.Input.KeyAction -= OnKeyboard; 
-        Managers.Input.KeyAction += OnKeyboard; 
+        Managers.Input.KeyAction += OnKeyboard;
+        Managers.Input.MouseAction -= OnMouseClicked; 
+        Managers.Input.MouseAction += OnMouseClicked; 
     }
 
     void Update()
     {
-
+        if(_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position; 
+            if(dir.magnitude < 0.0001f)
+            {
+                _moveToDest = false;
+            }
+            else
+            {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+                //if(moveDist >= dir.magnitude)
+                //    moveDist = dir.magnitude;
+                
+                transform.position += dir.normalized * moveDist; 
+                transform.LookAt(_destPos);
+            }
+        }
     }
     
     void OnKeyboard()
@@ -38,6 +60,26 @@ public class PlayerController : MonoBehaviour
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
             transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+
+        _moveToDest = false;
+    }
+
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (evt != Define.MouseEvent.Click)
+            return;
+
+        Debug.Log("OnMouseClicked");
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _moveToDest = true; 
         }
     }
 }
