239076fc1ba04bfee90380f047a23d0fc5e788ee
baljang <baljang7@gmail.com>
2022-09-09 15:31:31 +0900

    07_06_UI_UI 자동화 #3
    Q1. UI_Button 산하의 Image가 드래그 하면 움직이게 콜백 패턴을 이용해 구현해 보아라.
    
    UI_EventHandler 라는 스크립트 파일을 만들고, UI_Button 산하에 ItemIcon이라는 Image UI를 추가한다.
    
    Image를 Bind 하고, GetImage함수를 정의한다.
    
    UI_EventHandler스크립트에 IBeginDragHandler, IDragHandler 클래스를 상속 받고 구현한다.
    
    콜백 방식을 위해 <PointerEventData>를 받는 Action 변수인 OnBeginDragHandler와 IDragHandler를 public으로 선언한다.
    
     IBeginDrageHandler와 IDrageHandler클래스를 상속 받을 때 구현한 OnBeginDrag와 OnDrag 함수에 각각 OnBeginDrageHandler가 null이 아닐 때 OnDrageHandler가 null 아닐 때 PointerEventData를 인자를 전달하며 Invoke를 해준다.
    
    UI_Button에서 GetImage의 결과값을 gameObject로 받고, 거기에서 GetComponent로 UI_EventHandler를 추출한다. 그리고 OnDragHandler에  PointerEventData를 인자로 받는 람다 함수를 연동 시켜서 revoke 할 때 Image의 위치가 드래그한 대로 움직이게 한다.

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class UI_Button : UI_Base
@@ -24,13 +25,23 @@ public class UI_Button : UI_Base
         TestObject,
     }
 
+    enum Images
+    {
+        ItemIcon,
+    }
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons)); 
         Bind<TMP_Text>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects)); 
+        Bind<Image>(typeof(Images));
 
-        GetText((int)Texts.ScoreText).text = "Bind Test"; 
+        GetText((int)Texts.ScoreText).text = "Bind Test";
+
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+        UI_EventHandler evt = go.GetComponent<UI_EventHandler>();
+        evt.OnDragHandler += ((PointerEventData data) => { evt.gameObject.transform.position = data.position; }); 
     }
 
 
