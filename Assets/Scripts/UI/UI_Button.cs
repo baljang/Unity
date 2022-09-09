abc28ca9bda5dd7bd6a41c7d8894c4506b9b5544
baljang <baljang7@gmail.com>
2022-09-09 13:48:28 +0900

    07_05_UI_UI 자동화 #2
    Q1. 타입과 index를 넣어서 dictionary에서 원하는 오브젝트를 빼낼 수 있는 Get함수를 만들어 보세요.
    
    Q2. Bind 할 때 component 뿐만 아니라 Object도 할 수 있게 Object 전용 FindChild를 정의해 보세요.
    
    Q2. UI_Base 클래스를 생성하고 공통으로 사용될 부분을 옮긴 뒤 UI_Button이 상속하여 사용해 보세요.
    
    Q3. 간편하게 사용할 수 있게 GetText, GetButton, GetImage함수를 만들어 보세요.

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -5,9 +5,8 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UI_Button : MonoBehaviour
+public class UI_Button : UI_Base
 {
-    Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
 
     enum Buttons
     {
@@ -20,24 +19,20 @@ public class UI_Button : MonoBehaviour
         ScoreText,
     }
 
+    enum GameObjects
+    {
+        TestObject,
+    }
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons)); 
         Bind<TMP_Text>(typeof(Texts));
+        Bind<GameObject>(typeof(GameObjects)); 
+
+        GetText((int)Texts.ScoreText).text = "Bind Test"; 
     }
 
-    void Bind<T>(Type type) where T : UnityEngine.Object
-    {
-        string[] names = Enum.GetNames(type);
-
-        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
-        _objects.Add(typeof(T), objects); 
-
-        for(int i=0; i<names.Length; i++)
-        {
-            objects[i] = Util.FindChild<T>(gameObject, names[i], true); 
-        }
-    }
 
     int _score = 0; 
 
