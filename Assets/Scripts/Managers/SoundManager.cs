5a6397e21957390a899b31c5016931104dced372
baljang <baljang7@gmail.com>
2022-09-21 22:53:20 +0900

    09_01_Sound_SoundManager #1
    Q1. Sound의 3대 요소에 대해 말해 보세요
    
    Q2. TestSound 스크립트를 만들어서  OnTriggerEnter, GetComponent<AudioSource>(), PlayOneShot함수를 이용해서 Cube에 부딪히면 Sound의 길이 동안 재생이 된 다음 Destroy 되게 해보세요.
    
    Q3. 이렇게 하면 단점이 무엇인가요? 개선점은 무엇일까요?

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager  
+{
+    // MP3 Player  -> AudioSource
+    // MP3 À½¿ø    -> AudioCllip
+    // °ü°´(±Í)    -> AudioListener
+
+}
