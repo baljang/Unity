7b839591323ed802b84401bd6466d8705f59ebff
baljang <baljang7@gmail.com>
2022-09-22 18:01:07 +0900

    09_02_Sound_SoundManager #2
    Q1. SoundManager를 통해 소리가 나게 해보자.
    
    방법 요약: SoundManager에 Play 함수를 만든다.Define가서 Sound의 종류를 enum으로 만든다.
    
    SoundManager로 와서 AudioSource타입 _audioSources 배열을 만들고, new로 Define의 Sound enum의 MaxCount를 제외한 개수 만큼 배열을 만들게 한다.
    
    그리고 Play 함수를 정의한다. 매개 변수로 type, path, pitch를 받고 path에 /Sounds가 빠져 있으면 추가해준다. 일단 type이 Bgm인 경우는 나중에 처리해 주고, Bgm이 아닌 경우 이 경우는 Effect인 경우 AudioClip을 Load하고 enum의 Effect번재 _audioSources를 빼내서 넣어준 AudioSource 변수 audiosource의 pitch를 설정하고, PlayOneShot 함수로 audioClip을 재생한다.
    
    _audioSources배열을 채워주지 않았기에 Init에서 채워준다. @Sound라는 컴포넌트가 있나 체크하고 없으면 만들고, enum 갯수만큼 gameObject를 만들고, 거기에 AudioSource 컴포넌트를 붙이고,  @Sound를 부모로 한다. 그리고 _audioSources의 Bgm 번째는 loop를 켜준다.
    
    Managers로 가서 s_instance를 이용해 SoundManager의 Init을 실행합니다.
    
    그리고 TestSound 코드의 OnTriggerEnter에서 Managers를 이용해 Play 함수를 실행해줍니다.

--- Assets/Scripts/TestSound.cs
+++ Assets/Scripts/TestSound.cs
@@ -18,10 +18,13 @@ public class TestSound : MonoBehaviour
     public AudioClip audioClip2;
     private void OnTriggerEnter(Collider other)
     {
-        AudioSource audio = GetComponent<AudioSource>();
-        audio.PlayOneShot(audioClip); 
-        audio.PlayOneShot(audioClip2);
-        float lifeTime = Mathf.Max(audioClip.length, audioClip2.length); 
-        GameObject.Destroy(gameObject, lifeTime);
+        //AudioSource audio = GetComponent<AudioSource>();
+        //audio.PlayOneShot(audioClip); 
+        //audio.PlayOneShot(audioClip2);
+        //float lifeTime = Mathf.Max(audioClip.length, audioClip2.length); 
+        //GameObject.Destroy(gameObject, lifeTime);
+
+        Managers.Sound.Play(Define.Sound.Effect, "UnityChan/univ0001"); 
+        Managers.Sound.Play(Define.Sound.Effect, "UnityChan/univ0002"); 
     }
 }
