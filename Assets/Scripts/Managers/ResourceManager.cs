b79cd318794db817821178b9b6b1ecccded745ed
baljang <baljang7@gmail.com>
2022-09-26 14:30:41 +0900

    10_01_ObjectPooling_PoolManager #1
    Q1. Pooling할 대상인지 어떻게 구분하면 좋을까?
    
    Q2. ResourceManager의 어떤 부분을 개선하면 좋을까? 3가지 부분을 짚어보자.
    
    Q3. Poolable과 PoolManager 스크립트를 만들고,
    
    PoolManager에 Init, Push, Pop, GetOriginal 인터페이스를 만들고 Init에서는 @Pool_Root라는 pooling 된 오브젝트들이 종속될 부모 오브젝트를 생성한다. 그리고 Dictionary를 하나 만드는데 string이라는 키로 Pool이라는 클래스를 찾는 형태의 Dictionary다. @Pool_Root라는 PoolManager가 여러개의 Pool을 갖고 있는건데 string으로 이름을 찾아서 접근하는 거. 스샷에서 UnityChan_Root라는 애가 Pool이라는 클래스가 되는 거
    Pool이라는 클래스를 정의한다. Pool이란 클래스 안에는 프로퍼티로 GameObject Original, Transform Root를 만들고, Poolable타입의 Stack변수를 선언하고 new로 메모리를 할당한다.
    
    Root는 UnityChan을 산하에 위치시키고 싶으니까 정리차원에서 Root라는 걸 둔 거.
    
    poolable에는 그냥 boo타입의 IsUsing이라는 변수를 선언한다.

--- Assets/Scripts/Managers/ResourceManager.cs
+++ Assets/Scripts/Managers/ResourceManager.cs
@@ -11,17 +11,17 @@ public class ResourceManager
 
     public GameObject Instantiate(string path, Transform parent = null)
     {
+        // 1. original 이미 들고 있으면 바로 사용
         GameObject prefab = Load<GameObject>($"Prefabs/{path}");
         if (prefab == null)
         {
             Debug.Log($"Failed to load prefab : {path}");
             return null;
         }
-
+        
+        // 2. 혹시 풀링된 애가 있을까? 
         GameObject go = Object.Instantiate(prefab, parent);
-        int index = go.name.IndexOf("(Clone)");
-        if (index > 0)
-            go.name = go.name.Substring(0, index); 
+        go.name = prefab.name; 
 
         return go;  
     }
@@ -30,6 +30,9 @@ public class ResourceManager
     {
         if (go == null)
             return;
+
+        // 만약에 풀링이 필요한 아이라면 -> 풀링 매니져한테 위탁
+        // 당장 Destroy 하는게 아니라 내부적으로 숨겨 놔서 관리를 하다가 나중에 딴애가 필요하게 됐다고 하면 Intantiate의 2에서 뱉어주면 된다. 
 
         Object.Destroy(go);
     }
