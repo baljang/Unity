09d91bacf4fc6dde5d7462a694597e860ba538a6
baljang <baljang7@gmail.com>
2022-10-01 23:22:57 +0900

    12_02_Data(데이터)_Data Manager #2
    Q1. List를 같고 있는 StatData 클래스가 아닌 Dictionary<int, Stat>StatDict를 사용하도록 수정해 보자.
    
    Q2. 경로를 넣어서 Load해서 JsonUtility.FromJson으로 받아오는 부분을 LoadJson이란 함수로 만들다. Dictionary<Key, Value>를 반환하는 MakeValue라는 함수를 가진 ILoader<int, Value>라는 인터페이스를 선언하고 , StatData 클래스가 상속받게 한다. Init에서 호출할 LoadJson의 제네릭 인자로 넣을 StatData 클래스 안에 List<Stat> 데이터를 Dictionary<int, Stat>로 변환할 수 있는 MakeDict 함수를 정의한다. 그래서 Dictionary<int, Stat> StatDict 딕셔너리를 LoadJson이라는 제네릭 함수에 StatData, int, Stat을 넣고 매개 변수로 StatData를 넣고 MakeDict를 호출해 생성하게 코드를 정리한다.
    
    Q3. Scripts 산하에 Data라는 폴더를 만들고 Data.Contents라는 스크립트를 만들자. 그곳에 Stat과 StatData클래스를 옮기자.
    
    Q4. 새로운 데이터를 불러올 때 무엇을 추가하면 될까?
    
    Q5. GameScene에서 StatDict를 이용해 json 파일의 정보를 Dictionary로 받는 작업을 해보자.
    
    Q6. 이렇게 했을 때 장점은 무엇인가?

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -12,6 +12,8 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game; 
 
         Managers.UI.ShowSceneUI<UI_Inven>();
+
+        Dictionary<int, Stat> dict = Managers.Data.StatDict; 
     }
     
     public override void Clear()
