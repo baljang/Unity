6e61a5e48e72c9cc4cf75beb14735b3887a6df9d
baljang <baljang7@gmail.com>
2022-10-01 20:09:54 +0900

    12_01_Data(데이터)_Data Manager #1
    Q1. 수치들을 데이터로 관리하면 좋은 점 3가지를 말해 보시요.
    
    Q2. DataManager 스크립트를 만듭니다. Managers에 전역으로 쓸 수 있게 등록하고, Init에서 DataManager의 Init을 실행합니다. StatData.json 파일을 만들고 그 안에 레벨에 따른 스텟 정보를 입력합니다. DataManager의 Init에서 StatData.json의 정보를 Load합니다. string으로 Load된 정보들을 log로 출력합니다.
    
    Q3. Stat클래스를 만들고 Load된 정보 중에서 하나의 레벨에 해당하는 정보와 매칭 될 수 있게 변수들을 선언합니다. 그리고 StatData클래스를 만들고 Stat클래스의 List인 stats 변수를 만들고 new로 데이터를 할당합니다. 이렇게 Load한 데이터와 구조를 같게 만들고 각 클래스에 [Serializable]이라고 써줍니다. JsonUtility.FromJson<>() 함수를 이용해서 Load한 text정보를 StatData클래스에 넣습니다. 중단점을 잡고 정말로 클래스에 들어가는지 확인합니다.

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
-    Coroutine co;
 
     protected override void Init()
     {
@@ -13,31 +12,8 @@ public class GameScene : BaseScene
         SceneType = Define.Scene.Game; 
 
         Managers.UI.ShowSceneUI<UI_Inven>();
-
-        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
-        StartCoroutine("CoStopExplode", 2.0f); 
     }
-
-    IEnumerator CoStopExplode(float Seconds)
-    {
-        Debug.Log("Stop Enter");
-        yield return new WaitForSeconds(Seconds);
-        Debug.Log("Stop Excute!!!!");
-        if (co != null)
-        {
-            StopCoroutine(co);
-            co = null;
-        }
-    }
-
-    IEnumerator ExplodeAfterSeconds(float seconds)
-    {
-        Debug.Log("Explode Enter"); 
-        yield return new WaitForSeconds(seconds);
-        Debug.Log("Explode Excute!!!!");
-        co = null; 
-    }
-  
+    
     public override void Clear()
     {
 
