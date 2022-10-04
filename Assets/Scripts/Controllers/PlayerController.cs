b45d9fa8f0a251b03f3bbf136b8f9ce15f7314fa
baljang <baljang7@gmail.com>
2022-10-04 22:08:40 +0900

    13_02_미니 RPG_이동
    Q1. 움직이게 하려면 일단 Terrain에 TerrainCollider가 있어야 raytracing에 충돌이 되어 좌표를 찍을 수 있다.
    
    Q2. PlayerController에서 보면 Wall 레이어에만 반응하게 해 놓았으니 Terrain을 Wall로 레이어를 설정해야 한다.
    
    Q3. 카메라를 뒤로 빼준다.
    
    Q4. Window→AI→Navigation를 켜고 이동이 안되게 하고 싶은 곳에 위치한 오브젝트와 Terrain을 Static으로 한다. 그리고 bake를 누른다. 수치를 조정에 지붕위의 파란색도 없앤다.
    
    Q5. Player오브젝트에 nev mesh agent 컴포넌트를 추가한다. Player Controller 스크립트로 가서 UpdateMoving에서 NavMeshAgent 컴포넌트를  GetOrAdd해서 Move 함수를 이용해 목적지까지 가게 한다.
    
    Q6. 못가는 지역을 클릭하면 막혀도 계속 발을 움직이기 때문에 정밀도를 낮춰서 비슷하게 가까워지면 도착했다고 인식하게 수정하자.
    
    Q7. 이동 못하는 곳을 클릭하면 멈추지 않는 현상을 해결하기 위해 raycasting을 이용하기로 한다. Buildings를 비솟한 부딪히면 멈추게 할 오브젝트들은 새로운 Block이라는 Layer를 생성하고 Block으로 지정한다.
    
    Q8. PlayerController로 가서 플레이어의 위치에서 Raycast를 쏘고 부딪히면 상태를 Idle로 바꾼다. 플레이어의 발 위치에서 Vector3.up * 0.5를 더해서 허리 즈음에서 쏘게 한다. 이렇게 하면 Block 레이어를 가진 애에 부딪히면 멈추게 된다.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -31,24 +32,32 @@ public class PlayerController : MonoBehaviour
     {
         // 아무것도 안함
     }
-        void UpdateMoving()
+
+    void UpdateMoving()
     {
         Vector3 dir = _destPos - transform.position;
-        if (dir.magnitude < 0.0001f)
+        if (dir.magnitude < 1f)
         {
             _state = PlayerState.Idle;
         }
         else
         {
+            // TODO
+            NavMeshAgent nma = gameObject.GetOrAddComponent<NavMeshAgent>();
             float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
-            //if(moveDist >= dir.magnitude)
-            //    moveDist = dir.magnitude;                
-            transform.position += dir.normalized * moveDist;
+            nma.Move(dir.normalized * moveDist);
 
+            Debug.DrawRay(transform.position+Vector3.up * 0.5f, dir.normalized, Color.green); 
+            if (Physics.Raycast(transform.position+ Vector3.up * 0.5f, dir, 1.0f, LayerMask.GetMask("Block")))
+            {
+                _state = PlayerState.Idle; 
+                return;
+            }
+
+            // transform.position += dir.normalized * moveDist;
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
             transform.LookAt(_destPos);
         }
-
         // 애니메이션
         Animator anim = GetComponent<Animator>();
         // 현재 게임 상태에 대한 정보를 넘겨준다
@@ -108,14 +117,13 @@ public class PlayerController : MonoBehaviour
             return;
 
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+        //Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
         RaycastHit hit;
         if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
         {
             _destPos = hit.point;
             _state = PlayerState.Moving; 
-            //Debug.Log($"Raycast Camera @ {hit.collider.gameObject.tag}");
         }
     }
 
