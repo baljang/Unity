af5be0121994712652bfe0037917fa72567680f2
baljang <baljang7@gmail.com>
2022-10-08 23:16:42 +0900

    13_05_미니 RPG_타게팅 락온
    Q1. 클릭하고 있는 상태에서 커서를 움직이면 움직인 커서의 위치로 계속 이동을 하고, Monster를 클릭한 다음에 클릭을 유지하며 커서를 이동 하면 그대로 Monster에게 이동하게 해보자.
    Define의 enum MouseEvent로 가서 PointerDown, PointerUp을 추가한다.
    InputManager로 가서 float _pressedTime 변수를 추가하고, OnUpdate 함수에서 누르지 않았을 때 누르면 PointerDown, 누른 상태에서 누르는 거면 press, 0.2f 안에 눌렀다 떼면 click, 그거보다 길게 눌렀다 떼면 PointerUp을 매개변수로 넣어 MouseAction.Invoke한다.
    PlayerController로 가서 OnMouseClicked 함수의 이름을 OnMouseEvent로 바꾸고, MouseAction.Invoke했을 때 넣은 매개변수에 따라 switch문으로 분기를 만든다.
    PointerDown일 때는 일단 _destPos에 값을 넣고 _state도 Moving 상태가 되고, hit의 layer가 Monster면 _lockTarget에 hit의 object를 넣어준다. Monster가 아니면 _lockTarget에 null을 넣는다.
    Press일 때는 _lockTarget이 null이 아니면 _destPos를 _lockTarget의 position으로 설정하고, _lockTarget이 null이면 그냥 hit.point를 _destPos에 넣는다.
    PointerUp의 경우는 _lockTarget을 null로 한다.
    
    Q2. 누르고 있는 상태에서는 땅에서 시작해서 몬스터로 커서의 위치가 바뀌더라도 커서의 모양이 바뀌지 않게 수정해 보자.
    PlayerController의 UpdateMouseCursor에서 마우스를 누르고 있는 상태라면 return 하게 조건을 추가한다.
    
    Q3. 마우스를 누른 상태에서 막다른 길에 부딪히면 멈추는게 아니라 이동하려는 시도를 하게 바꿔보자.
    PlayerController의 UpdateMoving에서 부딪혔을 때 마우스를 안두르고 있을 때만 _state를 Idle로 바꾼다.
    
    Q4. 왜 유니티에서 제공하는 Input.만 쓰지 InputManager를 만들어서 쓰고 있는 걸까?

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -10,6 +10,7 @@ public class InputManager
     public Action<Define.MouseEvent> MouseAction = null; 
 
     bool _pressed = false; 
+    float _pressedTime = 0;
        
     void Start()
     {
@@ -28,14 +29,24 @@ public class InputManager
         {
             if(Input.GetMouseButton(0))
             {
+                if(!_pressed)
+                {
+                    MouseAction.Invoke(Define.MouseEvent.PointerDown);
+                    _pressedTime = Time.time; 
+                }
                 MouseAction.Invoke(Define.MouseEvent.Press);
                 _pressed = true; 
             }
             else
             {
                 if (_pressed)
-                    MouseAction.Invoke(Define.MouseEvent.Click);
-                _pressed = false; 
+                {
+                    if(Time.time < _pressedTime + 0.2f)
+                        MouseAction.Invoke(Define.MouseEvent.Click);
+                    MouseAction.Invoke(Define.MouseEvent.PointerUp);
+                }
+                _pressed = false;
+                _pressedTime = 0; 
             }
         }
     }
